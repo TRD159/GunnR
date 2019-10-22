c6c62dffc4918d3a0ec45058301f5d087a901e4d
OTHSCS163 <OTHSCS163@KATYISD.ORG>
2019-10-22 13:37:15 -0500

    Floor works

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -9,9 +9,10 @@ public class PlayerScript : MonoBehaviour
 
     public float dx = 0;
     public float dy = 0;
+    public float maxSpeed;
     public Vector2 position;
     Vector2 forces;
-    bool jump = true;
+    bool jump = false;
     bool grounded = false;
 
     public float speed;
@@ -31,20 +32,18 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
 
-        float hori = Input.GetAxis("Horizontal");
+        float hori = Input.GetAxis("Horizontal") * 3;
         float vert = 0;
         if(Input.GetAxis("Jump") > 0 && jump)
         {
-            vert = Input.GetAxis("Jump") * 10;
-            jump = false;
+            vert = Input.GetAxis("Jump") * 5;
+            //jump = false;
         }
         //float vert = ((Input.GetAxis("Vertical") < 0) ? 0 : Input.GetAxis("Vertical")) * speed;
 
-        if (hori == 0 && player.velocity.x != 0) {
-            forces = new Vector2(-forces.x, forces.y);
-        } else {
-            forces = new Vector2(hori, vert);
-        }
+
+        forces = new Vector2(hori, vert);
+        
 
         //forces = new Vector2(hori, vert);
         position = player.transform.position;
@@ -52,6 +51,28 @@ public class PlayerScript : MonoBehaviour
         //player.AddForce(new Vector2(hori, vert));
 
         player.velocity += forces;
+        if(player.velocity.x > maxSpeed)
+        {
+            player.velocity = new Vector2(maxSpeed, player.velocity.y);
+        }
+        if (player.velocity.x < -maxSpeed)
+        {
+            player.velocity = new Vector2(-maxSpeed, player.velocity.y);
+        }
+
+        if(hori == 0)
+        {
+            if(player.velocity.x > 1)
+            {
+                player.velocity = new Vector2(player.velocity.x - 1, player.velocity.y);
+            } else if(player.velocity.x < -1)
+            {
+                player.velocity = new Vector2(player.velocity.x + 1, player.velocity.y);
+            } else
+            {
+                player.velocity = new Vector2(0, player.velocity.y);
+            }
+        }
 
         /*if(player.velocity.magnitude > 5)
         {
@@ -61,12 +82,17 @@ public class PlayerScript : MonoBehaviour
         dx = player.velocity.x;
         dy = player.velocity.y;
 
-        Debug.Log(dx + ", " + dy);
+        //Debug.Log(dx + ", " + dy);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("A");
         jump = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        jump = false;
     }
 }
